ac85a26d9e2d89c264b1b0f8d0b9270c2bec75b1
Michael Esclavy <michael.esclavy@ynov.com>
2026-02-23 15:18:34 +0100

    quete 5

--- quete-5.sql
+++ quete-5.sql
@@ -3,3 +3,11 @@ FROM vehicules
 WHERE etat = 'Disponible'
 ORDER BY autonomie_km DESC
 LIMIT 3;
+
+SELECT localisation AS ville, COUNT(*) AS nb_vehicules
+FROM vehicules
+WHERE etat = 'Disponible'
+GROUP BY localisation
+ORDER BY nb_vehicules DESC
+LIMIT 1;
+
