4ead81a69e40d872cf3d3e2717c1b854eb24fffb
Michael Esclavy <michael.esclavy@ynov.com>
2026-02-25 23:37:54 +0100

    quete 5

--- quete-5.sql
+++ quete-5.sql
@@ -11,3 +11,13 @@ GROUP BY localisation
 ORDER BY nb_vehicules DESC
 LIMIT 1;
 
+SELECT c.id_client, c.nom, c.prenom, COUNT(l.id_location) AS nb_locations
+FROM client c
+JOIN location l ON c.id_client = l.id_client
+GROUP BY c.id_client, c.nom, c.prenom
+HAVING COUNT(l.id_location) >= 2;
+
+SELECT v.id, v.marque, v.modele
+FROM vehicules v
+LEFT JOIN location l ON v.id = l.id_vehicule
+WHERE l.id_vehicule IS NULL;
